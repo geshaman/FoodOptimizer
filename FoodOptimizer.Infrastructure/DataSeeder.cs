56a052b00af86b89fa13475194bd2a1c998f1786
geshaman <glebovyaroslav58@gmail.com>
2026-05-31 14:51:18 +0300

    Добавил в DataSeeder ещё данных

--- FoodOptimizer.Infrastructure/DataSeeder.cs
+++ FoodOptimizer.Infrastructure/DataSeeder.cs
@@ -41,14 +41,79 @@ namespace FoodOptimizer.Infrastructure
                     },
                     new MenuItem
                     {
-                        Name = "Картофель фри Средний",
+                        Name = "Биг Хит",
+                        Price = 239,
+                        Category = Category.Entree,
+                        Calories = 524,
+                        Discounts = new List<Discount>{
+                            new Discount
+                            {
+                                DiscountType = DiscountType.Percentage,
+                                Value = 20,
+                                ActivationType = ActivationType.QrCode,
+                                Start = new TimeOnly(9, 0),
+                                End = new TimeOnly(23, 0)
+                            }
+                        }
+                    },
+                    new MenuItem
+                    {
+                        Name = "Чикен Премьер",
+                        Price = 239,
+                        Category = Category.Entree,
+                        Calories = 525,
+                        Discounts = new List<Discount>{
+                            new Discount
+                            {
+                                DiscountType = DiscountType.FixedPrice,
+                                Value = 50,
+                                ActivationType = ActivationType.Automatic,
+                                Start = new TimeOnly(9, 0),
+                                End = new TimeOnly(23, 0)
+                            }
+                        }
+                    },
+                    new MenuItem
+                    {
+                        Name = "Картофель Фри Средний",
                         Price = 117,
                         Category = Category.Garnish,
                         Calories = 318
                     },
                     new MenuItem
                     {
+                        Name = "Наггетсы 6 шт.",
+                        Price = 107,
+                        Category = Category.Garnish,
+                        Calories = 268,
+                        Discounts = new List<Discount>{
+                            new Discount
+                            {
+                                DiscountType = DiscountType.FixedPrice,
+                                Value = 107-99,
+                                ActivationType = ActivationType.Automatic,
+                                Start = new TimeOnly(9, 0),
+                                End = new TimeOnly(23, 0)
+                            }
+                        }
+                    },
+                    new MenuItem
+                    {
                         Name = "Добрый Кола Средний",
+                        Price = 146,
+                        Category = Category.Drinks,
+                        Calories = 106
+                    },
+                    new MenuItem
+                    {
+                        Name = "Апельсиновый Сок Средний",
+                        Price = 162,
+                        Category = Category.Drinks,
+                        Calories = 187
+                    },
+                    new MenuItem
+                    {
+                        Name = "Липтон Зелёный Средний",
                         Price = 146,
                         Category = Category.Drinks,
                         Calories = 106
