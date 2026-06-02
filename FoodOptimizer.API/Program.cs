da13848af9ed07bc59c8b3321706b2da1e928a9b
geshaman <glebovyaroslav58@gmail.com>
2026-06-03 00:30:39 +0300

    Подправил Program.cs

--- FoodOptimizer.API/Program.cs
+++ FoodOptimizer.API/Program.cs
@@ -39,11 +39,10 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
